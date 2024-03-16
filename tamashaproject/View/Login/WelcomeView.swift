3535a9dc5cd649d657333a40ca4d9c5993e52767
tamashathiruni <thirunitamasha@gmail.com>
2024-03-16 14:46:52 +0530

    welcome page

---
+++ tamashaproject/View/Login/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  tamashaproject
+//
+//  Created by NIBMPC04PC02 on 2024-03-16.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
