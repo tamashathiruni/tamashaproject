b790760e5554abe49d597e02c29963921229eb55
tamashathiruni <thirunitamasha@gmail.com>
2024-03-16 13:12:19 +0530

    Initial Commit

---
+++ tamashaproject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tamashaproject
+//
+//  Created by NIBMPC04PC02 on 2024-03-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello !")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tamashaproject/tamashaprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamashaprojectApp.swift
+//  tamashaproject
+//
+//  Created by NIBMPC04PC02 on 2024-03-16.
+//
+
+import SwiftUI
+
+@main
+struct tamashaprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
